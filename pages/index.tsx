057588f9a4e00c4b29588ddb994b4e2f3bda7a60
Jiae Lee <leejiae@vtstechlab.com>
2022-05-11 00:26:14 +0900

    Update posts page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,42 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { getSortedPostsData } from '../lib/posts'
 import homeStyles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-const Home: NextPage = () => {
+const Home = ({ allPostsData }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) => {
   return (
-    <div>
+    <div className={homeStyles.container}>
       <Head>
-        <title>Jiae</title>
+        <title>Blog</title>
       </Head>
 
       <section className={homeStyles.headingMd}>
-        <p>[Jiae Introduction]</p>
+        <p>NextJs & TypeScript</p>
         <p>(This is a website)</p>
       </section>
 
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
-        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <h2 className={homeStyles.headingLg}>Posts</h2>
         <ul className={homeStyles.list}>
-
+          {allPostsData.map(({ id, title, date }) =>
+            <li className={homeStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={homeStyles.lightText}>
+                {date}
+              </small>
+            </li>
+          )}
         </ul>
       </section>
     </div>
@@ -26,3 +44,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
